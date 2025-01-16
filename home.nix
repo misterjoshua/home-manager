8460b94d887b3d522b41e4e3abc1d4154ee051a5
Josh Kellendonk <joshkellendonk@gmail.com>
2025-01-15 19:39:30 -0700

    configure the shell

--- home.nix
+++ home.nix
@@ -61,9 +61,6 @@
     httpie
     wget
     ngrok
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
@@ -94,26 +91,24 @@
     # '';
   };
 
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. These will be explicitly sourced when using a
-  # shell provided by Home Manager. If you don't want to manage your shell
-  # through Home Manager then you have to manually source 'hm-session-vars.sh'
-  # located at either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/josh/etc/profile.d/hm-session-vars.sh
-  #
   home.sessionVariables = {
-    # EDITOR = "emacs";
+    EDITOR = "vim";
   };
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
+  
+  # Manage bash's config.
+  programs.bash.enable = true;
+  programs.bash.shellAliases = {
+    ls = "ls --color=auto";
+    grep = "grep --color=auto";
+    diff = "diff --color=auto";
+  };
+
+  # Powerline bash prompt line
+  programs.powerline-go.enable = true;
+  programs.powerline-go.settings = {
+    mode = "flat"; # flat so it works in more terminals
+  };
 }
