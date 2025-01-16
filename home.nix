015f341e92a405dee7c752aff500b21f720b8756
Josh Kellendonk <joshkellendonk@gmail.com>
2025-01-15 20:29:21 -0700

    add tmux

--- home.nix
+++ home.nix
@@ -63,6 +63,7 @@
     ngrok
     tree
     htop
+    tmux
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
