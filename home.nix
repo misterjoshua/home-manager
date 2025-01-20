32fd28acbb2906b13e682d1a3cb450086d2e0876
Josh Kellendonk <joshkellendonk@gmail.com>
2025-01-19 20:45:18 -0700

    Revert "remove vim configs - doing in different repo"
    This reverts commit 89e430b3160ea70a2d4b054051a158d50fbd4072.

--- home.nix
+++ home.nix
@@ -89,6 +89,7 @@
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
+    ".vimrc".source = dotfiles/vimrc;
     # # Building this configuration will create a copy of 'dotfiles/screenrc' in
     # # the Nix store. Activating the configuration will then make '~/.screenrc' a
     # # symlink to the Nix store copy.
@@ -153,6 +154,10 @@
   # Kubectl
   programs.kubecolor.enable = true;
   
-  # Neovim
-  programs.neovim.enable = true;
+  programs.neovim = {
+    enable = true;
+    plugins = with pkgs.vimPlugins; [
+      vim-plug
+    ];
+  };
 }
