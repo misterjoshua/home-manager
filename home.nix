89e430b3160ea70a2d4b054051a158d50fbd4072
Josh Kellendonk <joshkellendonk@gmail.com>
2025-01-19 20:40:53 -0700

    remove vim configs - doing in different repo

--- home.nix
+++ home.nix
@@ -89,7 +89,6 @@
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
-    ".vimrc".source = dotfiles/vimrc;
     # # Building this configuration will create a copy of 'dotfiles/screenrc' in
     # # the Nix store. Activating the configuration will then make '~/.screenrc' a
     # # symlink to the Nix store copy.
@@ -153,10 +152,6 @@
   # Kubectl
   programs.kubecolor.enable = true;
   
-  programs.neovim = {
-    enable = true;
-    plugins = with pkgs.vimPlugins; [
-      vim-plug
-    ];
-  };
+  # Neovim
+  programs.neovim.enable = true;
 }
