fd6ea66a614600eb0cb8da270c4d18a86a28b585
Josh Kellendonk <joshkellendonk@gmail.com>
2025-01-29 18:40:28 -0700

    add graphviz

--- home.nix
+++ home.nix
@@ -78,6 +78,7 @@
     htop
     tmux
     direnv
+    graphviz
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
