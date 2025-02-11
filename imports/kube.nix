6097d49cb35660a4a35a242050c5f025c56a69cf
Josh Kellendonk <joshkellendonk@gmail.com>
2025-02-11 13:58:58 -0700

    add missing aliases

--- imports/kube.nix
+++ imports/kube.nix
@@ -24,6 +24,16 @@
   programs.bash.initExtra = ''
     source <(kubectl completion bash)
     complete -o default -F __start_kubectl k
+    source <(velero completion bash)
+    complete -o default -F __start_velero v
+
+    function pxctl() {
+      if [ -z "$PX_POD" ]; then
+        export PX_POD=$(kubectl -n kube-system get pods -lname=portworx -o jsonpath='{.items[0].metadata.name}')
+      fi
+
+      kubectl -n kube-system exec -it "$PX_POD" -- /opt/pwx/bin/pxctl "$@"
+    }
   '';
 
   # Create symlinks for kubectl plugins
