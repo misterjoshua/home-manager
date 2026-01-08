426a2590c2542aaa9debc3ae39911b60a7b8bd54
Josh Kellendonk <joshkellendonk@gmail.com>
2026-01-07 21:18:20 -0700

    add flake

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "Home Manager Flake";
+
+  nixConfig = {
+    experimental-features = [ "nix-command" "flakes" ];
+  };
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }: {
+    homeConfigurations.josh =  home-manager.lib.homeManagerConfiguration {
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      modules = [ ./home.nix ];
+    };
+  };
+}
