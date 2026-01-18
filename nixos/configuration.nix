5676e6e17170669cec0e2e47fa75ed2358c7101f
Josh Kellendonk <joshkellendonk@gmail.com>
2026-01-17 22:23:30 -0700

    enable mdns/avahi

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -19,6 +19,18 @@
   services.displayManager.sddm.enable = true;
   services.desktopManager.plasma6.enable = true;
 
+  services.avahi = {
+    enable = true;
+    nssmdns4 = true;
+    publish = {
+      enable = true;
+      addresses = true;
+      workstation = true;
+    };
+
+    openFirewall = true;
+  };
+
   # Enable sound with pipewire.
   services.pulseaudio.enable = false;
   security.rtkit.enable = true;
