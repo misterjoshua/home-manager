437ad2e9b6340e220fb26d3f0de85d47222e60b5
Josh Kellendonk <joshkellendonk@gmail.com>
2026-01-20 18:45:59 -0700

    add nixd

--- shells.nix
+++ shells.nix
@@ -4,7 +4,10 @@
     { pkgs, ... }:
     {
       devShells.default = pkgs.mkShell {
-        packages = with pkgs; [ nixfmt ];
+        packages = with pkgs; [
+          nixfmt
+          nixd
+        ];
       };
     };
 }
