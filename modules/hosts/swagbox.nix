a52b57a605a69638747aefb61d222e9003c7f1e5
Josh Kellendonk <joshkellendonk@gmail.com>
2026-01-28 12:47:53 -0700

    readd base

--- modules/hosts/swagbox.nix
+++ modules/hosts/swagbox.nix
@@ -6,7 +6,7 @@
     system = "x86_64-linux";
     hardware = ./_hardware/swagbox.nix;
     hostname = "swagbox";
-    features.desktop.enable = true;
+    features.base.enable = true;
     features.josh.enable = true;
   };
 }
