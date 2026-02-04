284ada337863d77b0d3eee90bfd18a1735a56677
Josh Kellendonk <joshkellendonk@gmail.com>
2026-02-03 21:17:00 -0700

    add precommit

--- modules/project.nix
+++ modules/project.nix
@@ -1,9 +1,25 @@
-{ inputs, config, ... }:
+{ ... }:
 {
   perSystem =
-    { system, pkgs, ... }:
+    { config, pkgs, ... }:
     {
       pre-commit.settings.hooks.nixfmt.enable = true;
       formatter = pkgs.nixfmt-tree;
+
+      devShells.default = pkgs.mkShell {
+        packages =
+          with pkgs;
+          [
+            nixfmt
+            nixd
+            nixos-rebuild
+            age
+          ]
+          ++ config.pre-commit.settings.enabledPackages;
+        shellHook = ''
+          ${config.pre-commit.shellHook}
+          echo "Welcome to the Dev Shell"
+        '';
+      };
     };
 }
