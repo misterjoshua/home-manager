86f8939822cd484a70407c502c880ca82623311a
Josh Kellendonk <joshkellendonk@gmail.com>
2026-04-24 19:44:10 +0200

    add chromium to wsl

--- modules/features/home-manager/wsl.nix
+++ modules/features/home-manager/wsl.nix
@@ -1,7 +1,11 @@
 {
   flake.modules.homeManager.wsl =
-    { ... }:
+    { pkgs, ... }:
     {
+      home.packages = with pkgs; [
+        chromium
+      ];
+
       targets.genericLinux.enable = true;
       targets.genericLinux.gpu.enable = false;
       home.shellAliases = {
